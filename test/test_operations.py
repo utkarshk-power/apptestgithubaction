f3b76c65c969188a0eddb319417a0d0cc68acaec
Urkarsh Kulshrestha <urkarsh.kulshrestha@px-01164.local>
2025-12-12 20:26:08 +0900

    Unit Test Cases Updates

---
+++ test/test_operations.py
@@ -0,0 +1,15 @@
+from src.math_operations import add, subtract
+
+
+def test_add():
+    assert add(2,3) == 5
+    assert add(-1,1) == 0
+    assert add(-2,-3) == -5
+  
+
+def test_subtract():
+    assert subtract(5,3) == 2
+    assert subtract(0,0) == 0
+    assert subtract(-2,-3) == 1
+    assert subtract(2,5) == -3    
+    assert subtract(-1,1) == -2
